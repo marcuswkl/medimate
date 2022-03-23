c04ccf39aeba577098574728a06a3b74cd343b24
Ko Jia Xin <kojiaxin@hotmail.com>
2022-03-23 21:42:07 +0800

    Comment unnecessary lines

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-Home main.dart
 import 'package:flutter/material.dart';
-import 'view/search/how-to-swallow.dart';
+import 'view/home/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const HowToSwallowWidget(),
+      home: HomePageWidget(key: UniqueKey()),
     );
   }
 }
