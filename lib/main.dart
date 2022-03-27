723421b1e201727329ff27e4ad6d8c95b83787ec
Daniel <82069022+19074426@users.noreply.github.com>
2022-03-27 15:51:55 +0800

    Update dart files
    Debugging.

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'view/medication-details/add_medication_1.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override 
   Widget build(BuildContext context) {
     return const MaterialApp(
