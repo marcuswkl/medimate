e85728a6a7a325fa0f5b60c2d5f528d89430a02d
Marcus Wong Ke Lun <sucramgnow0110@gmail.com>
2022-04-01 02:42:12 +0800

    Add front end code for profile setup 2 page

---
+++ lib/view/signup/profile_setup/profile_setup_2.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+// import '../time_setup1/time_setup1_widget.dart';
+
+class ProfileSetup2Widget extends StatefulWidget {
+  const ProfileSetup2Widget({Key? key}) : super(key: key);
+
+  @override
+  _ProfileSetup2WidgetState createState() => _ProfileSetup2WidgetState();
+}
+
+class _ProfileSetup2WidgetState extends State<ProfileSetup2Widget> {
+  late TextEditingController textController;
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    textController = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Padding(
+                    padding:
+                        const EdgeInsetsDirectional.fromSTEB(20, 20, 0, 20),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: Color(0xFF809BCE),
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      child: IconButton(
+                        iconSize: 30,
+                        onPressed: () async {
+                          Navigator.pop(context);
+                        },
+                        icon: const Icon(
+                          Icons.chevron_left,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(0, 30, 0, 30),
+                        child: Image.asset(
+                          'assets/images/professor.png',
+                          width: 221,
+                          height: 156,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height * 0.6275,
+                        decoration: const BoxDecoration(
+                          color: Color(0xFF809BCE),
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(0),
+                            bottomRight: Radius.circular(0),
+                            topLeft: Radius.circular(66),
+                            topRight: Radius.circular(66),
+                          ),
+                        ),
+                        child: Padding(
+                          padding:
+                              const EdgeInsetsDirectional.fromSTEB(0, 50, 0, 0),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsetsDirectional.fromSTEB(
+                                    0, 0, 0, 20),
+                                child: Container(
+                                  width: 281,
+                                  height: 58,
+                                  decoration: const BoxDecoration(),
+                                  child: Padding(
+                                    padding:
+                                        const EdgeInsetsDirectional.fromSTEB(
+                                            0, 0, 0, 2),
+                                    child: Text(
+                                      'What are your current drug allergies?',
+                                      style: GoogleFonts.signikaNegative(
+                                        color: Colors.black,
+                                        fontSize: 21,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsetsDirectional.fromSTEB(
+                                    0, 0, 0, 20),
+                                child: Container(
+                                  width: 295,
+                                  height: 229,
+                                  decoration: const BoxDecoration(
+                                    color: Colors.white54,
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(11.0),
+                                    ),
+                                  ),
+                                  child: TextFormField(
+                                    controller: textController,
+                                    obscureText: false,
+                                    decoration: const InputDecoration(
+                                      hintText: 'Eg: Penicilin',
+                                      enabledBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                          color: Colors.black,
+                                          width: 1,
+                                        ),
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(11.0),
+                                        ),
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                          color: Colors.black,
+                                          width: 1,
+                                        ),
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(11.0),
+                                        ),
+                                      ),
+                                    ),
+                                    style: GoogleFonts.signikaNegative(),
+                                  ),
+                                ),
+                              ),
+                              Row(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Padding(
+                                    padding:
+                                        const EdgeInsetsDirectional.fromSTEB(
+                                            0, 0, 50, 50),
+                                    child: ElevatedButton(
+                                      onPressed: () async {
+                                        await Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                Column(), //TODO: Replace with TimeSetupBreakfastWidget(),
+                                          ),
+                                        );
+                                      },
+                                      style: ButtonStyle(
+                                        backgroundColor:
+                                            MaterialStateProperty.all<Color>(
+                                                const Color(0xFFFFFEFE)),
+                                        minimumSize:
+                                            MaterialStateProperty.all<Size>(
+                                                const Size(103, 35)),
+                                        shape: MaterialStateProperty.all<
+                                                OutlinedBorder>(
+                                            RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(20))),
+                                      ),
+                                      child: Text(
+                                        'Next',
+                                        style: GoogleFonts.signikaNegative(
+                                          color: Colors.black,
+                                          fontSize: 18,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
