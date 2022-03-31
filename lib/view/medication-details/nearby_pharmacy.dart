8b240a0a8303e86919daecd81106a8c84158c666
Daniel <82069022+19074426@users.noreply.github.com>
2022-03-31 16:00:05 +0800

    Update nearby_pharmacy.dart

--- lib/view/medication-details/nearby_pharmacy.dart
+++ lib/view/medication-details/nearby_pharmacy.dart
@@ -97,7 +97,7 @@ class _NearbyPharmacyWidgetState extends State<NearbyPharmacyWidget> {
                   alignment: const AlignmentDirectional(0, 0),
                   child: Container(
                     height: MediaQuery.of(context).size.height * 1,
-                    decoration: BoxDecoration(),
+                    decoration: const BoxDecoration(),
                     child: FlutterFlowGoogleMap(
                       controller: googleMapsController,
                       onCameraIdle: (latLng) =>
