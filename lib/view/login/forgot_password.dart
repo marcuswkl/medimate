72c377227d6ad6f65ef4515e3cea6c4e3a2e12c2
Marcus Wong Ke Lun <sucramgnow0110@gmail.com>
2022-03-31 19:36:29 +0800

    Add front end code for forgot password page

---
+++ lib/view/login/forgot_password.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ForgotPasswordWidget extends StatefulWidget {
+  const ForgotPasswordWidget({Key? key}) : super(key: key);
+
+  @override
+  _ForgotPasswordWidgetState createState() => _ForgotPasswordWidgetState();
+}
+
+class _ForgotPasswordWidgetState extends State<ForgotPasswordWidget> {
+  late TextEditingController textController;
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    textController = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(20, 20, 0, 20),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: Color(0xFF809BCE),
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      child: IconButton(
+                        iconSize: 30,
+                        onPressed: () async {
+                          Navigator.pop(context);
+                        },
+                        icon: const Icon(
+                          Icons.chevron_left,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 20),
+                        child: Text(
+                          'Forgot Password',
+                          style: GoogleFonts.fredokaOne(
+                            color: Colors.black,
+                            fontSize: 30,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 20),
+                        child: Text(
+                          'Submit your email to\nreceive instructions to reset password',
+                          style: GoogleFonts.signikaNegative(
+                            color: Colors.black,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 20),
+                        child: Image.asset(
+                          'assets/images/forgot_password.png',
+                          width: 221,
+                          height: 147,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        width: 278,
+                        height: 26,
+                        decoration: BoxDecoration(),
+                        child: Text(
+                          'Email',
+                          style: GoogleFonts.signikaNegative(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 20),
+                        child: Container(
+                          width: 278,
+                          height: 52,
+                          decoration: BoxDecoration(),
+                          child: Padding(
+                            padding: const EdgeInsetsDirectional.fromSTEB(
+                                0, 0, 0, 2),
+                            child: TextFormField(
+                              controller: textController,
+                              obscureText: false,
+                              decoration: const InputDecoration(
+                                hintText: 'Eg: example@gmail.com',
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.black45,
+                                    width: 1,
+                                  ),
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(4.0),
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.black,
+                                    width: 1,
+                                  ),
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(4.0),
+                                  ),
+                                ),
+                              ),
+                              style: GoogleFonts.signikaNegative(
+                                // color: Colors.black,
+                                // fontSize: 18,
+                                ),
+                              keyboardType: TextInputType.emailAddress,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 20),
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            //TODO: Forgot Password API
+                          },
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                const Color(0xFF809BCE)),
+                            minimumSize: MaterialStateProperty.all<Size>(
+                                const Size(287, 45)),
+                            shape: MaterialStateProperty.all<OutlinedBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5))),
+                          ),
+                          child: Text(
+                            'Send Instructions',
+                            style: GoogleFonts.signikaNegative(
+                              color: Colors.white,
+                              fontSize: 22,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
