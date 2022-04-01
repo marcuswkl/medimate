6499e33fe8334c56102b87b4015305e71ab51298
Marcus Wong Ke Lun <sucramgnow0110@gmail.com>
2022-04-01 08:29:36 +0800

    Duplicate breakfast front end code for lunch and dinner setup

---
+++ lib/view/signup/time_setup/time_setup_dinner.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TimeSetupDinnerWidget extends StatefulWidget {
+  const TimeSetupDinnerWidget({Key? key}) : super(key: key);
+
+  @override
+  _TimeSetupDinnerWidgetState createState() => _TimeSetupDinnerWidgetState();
+}
+
+class _TimeSetupDinnerWidgetState extends State<TimeSetupDinnerWidget> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Padding(
+                    padding: const EdgeInsetsDirectional.fromSTEB(20, 20, 0, 20),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: Color(0xFF809BCE),
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      child: IconButton(
+                        iconSize: 30,
+                        onPressed: () async {
+                          Navigator.pop(context);
+                        },
+                        icon: const Icon(
+                          Icons.chevron_left,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsetsDirectional.fromSTEB(0, 30, 0, 30),
+                        child: Image.asset(
+                          'assets/images/time_setup.png',
+                          width: 169,
+                          height: 158,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height * 0.525,
+                        decoration: const BoxDecoration(
+                          color: Color(0xFF023E8A),
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(0),
+                            bottomRight: Radius.circular(0),
+                            topLeft: Radius.circular(66),
+                            topRight: Radius.circular(66),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsetsDirectional.fromSTEB(0, 40, 0, 0),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Padding(
+                                padding:
+                                    const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 20),
+                                child: Container(
+                                  width: 281,
+                                  height: 58,
+                                  decoration: const BoxDecoration(),
+                                  child: Padding(
+                                    padding: const EdgeInsetsDirectional.fromSTEB(
+                                        0, 0, 0, 2),
+                                    child: Text(
+                                      'Please enter the time that you usually have your:',
+                                      style: GoogleFonts.signikaNegative(
+                                        color: Colors.black,
+                                        fontSize: 21,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                width: 282,
+                                height: 32,
+                                decoration: BoxDecoration(
+                                  // color: const Color(0xFFEEEEEE),
+                                ),
+                                child: Text(
+                                  'Dinner',
+                                  style: GoogleFonts.signikaNegative(
+                                        color: Colors.black,
+                                        fontSize: 21,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                ),
+                              ),
+                              Row(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsetsDirectional.fromSTEB(
+                                        0, 0, 50, 50),
+                                    child: ElevatedButton(
+                                      onPressed: () async {
+                                        await Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                Column(), //TODO: Replace with TimeSetupLunchWidget(),
+                                          ),
+                                        );
+                                      },
+                                      style: ButtonStyle(
+                                        backgroundColor:
+                                            MaterialStateProperty.all<Color>(
+                                                const Color(0xFFFFFEFE)),
+                                        minimumSize:
+                                            MaterialStateProperty.all<Size>(
+                                                const Size(103, 35)),
+                                        shape: MaterialStateProperty.all<
+                                                OutlinedBorder>(
+                                            RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(20))),
+                                      ),
+                                      child: Text(
+                                        'Next',
+                                        style: GoogleFonts.signikaNegative(
+                                          color: Colors.black,
+                                          fontSize: 18,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
